674f1cc236423d485f4b7f4947fb94f85d573c22
Laura <lpadgett23@gmail.com>
2024-06-17 12:41:46 -0400

    python modules demo app - pulling requests from github api

---
+++ python-modules/github-requests/main.py
@@ -0,0 +1,13 @@
+import requests
+
+response = requests.get("https://api.github.com/repos/lpadgett23/techwnana-devops-bootcamp/issues")
+# print(response.text)
+# print(response.json)
+# print(type(response.json))
+# print(response.json()[0])
+
+my_issues = response.json()
+
+for issue in my_issues:
+    print(f"Description of Issue: {issue['title']}\n Status: {issue['state']}\n")
+
